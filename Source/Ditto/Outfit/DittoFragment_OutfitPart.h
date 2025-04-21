528fd8d5829553d9a93abdd94f4897f54241a720
tarnishablec <tarnishablec@outlook.com>
2025-04-21 16:49:28 +0800

    DisplayName OutfitPart

--- Source/Ditto/Outfit/DittoFragment_OutfitPart.h
+++ Source/Ditto/Outfit/DittoFragment_OutfitPart.h
@@ -13,30 +13,30 @@ class UCombeeContainer;
 /**
  * 
  */
-UCLASS(Abstract)
+UCLASS(Abstract, DisplayName="Outfit Part (Ditto)")
 class DITTO_API UDittoFragment_OutfitPart : public UCombeeFragment, public ICombeeUsableFragment
 {
-	GENERATED_BODY()
+    GENERATED_BODY()
 
 public:
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Outfit")
-	FGameplayTagContainer Part;
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Ditto | Outfit")
+    FGameplayTagContainer Part;
 
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Outfit")
-	TArray<TSubclassOf<UCombeeContainer>> Closets;
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Ditto | Outfit")
+    TArray<TSubclassOf<UCombeeContainer>> Closets;
 
-	virtual FVoidCoroutine OnUse(AController* const& Instigator, TInstancedStruct<FCombeeUsePayload>& Payload,
-	                             bool& bSuccess, const UE5Coro::TLatentContext<> _) override;
+    virtual FVoidCoroutine OnUse(AController* const& Instigator, TInstancedStruct<FCombeeUsePayload>& Payload,
+                                 bool& bSuccess, const UE5Coro::TLatentContext<> _) override;
 
-	UFUNCTION(NetMulticast, Reliable, BlueprintCallable, Category="Outfit Avatar")
-	void TakeOff(const FInstancedStruct& PartData);
+    UFUNCTION(NetMulticast, Reliable, BlueprintCallable, Category="Ditto | Outfit Avatar")
+    void TakeOff(const FInstancedStruct& PartData);
 
-	UFUNCTION(BlueprintNativeEvent, Category="Outfit Avatar", DisplayName="TakeOff")
-	void K2_TakeOff(const FInstancedStruct& PartData);
+    UFUNCTION(BlueprintNativeEvent, Category="Ditto | Outfit Avatar", DisplayName="TakeOff")
+    void K2_TakeOff(const FInstancedStruct& PartData);
 
-	UFUNCTION(NetMulticast, Reliable, BlueprintCallable, Category="Outfit Avatar")
-	void Wear(const FInstancedStruct& PartData);
+    UFUNCTION(NetMulticast, Reliable, BlueprintCallable, Category="Ditto | Outfit Avatar")
+    void Wear(const FInstancedStruct& PartData);
 
-	UFUNCTION(BlueprintNativeEvent, Category="Outfit Avatar", DisplayName="Wear")
-	void K2_Wear(const FInstancedStruct& PartData);
+    UFUNCTION(BlueprintNativeEvent, Category="Ditto | Outfit Avatar", DisplayName="Wear")
+    void K2_Wear(const FInstancedStruct& PartData);
 };
