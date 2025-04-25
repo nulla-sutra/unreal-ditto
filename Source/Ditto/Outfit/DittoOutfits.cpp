18c80ea396e627f31ac259890dcaf7ea8c83f369
tarnishablec <tarnishablec@outlook.com>
2025-04-25 17:24:57 +0800

    Outfits Compatible check

--- Source/Ditto/Outfit/DittoOutfits.cpp
+++ Source/Ditto/Outfit/DittoOutfits.cpp
@@ -4,65 +4,69 @@
 #include "DittoOutfits.h"
 
 #include "BlueprintGameplayTagLibrary.h"
+#include "DittoFragment_OutfitPart.h"
 #include "DittoOutfitAvatar.h"
+#include "Combee/Core/Item/CombeeItemInterface.h"
+#include "Combee/Helpers/CombeeLibrary.h"
 #include "Net/UnrealNetwork.h"
 
 
 // Sets default values for this component's properties
 UDittoOutfits::UDittoOutfits()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
+    // Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+    // off to improve performance if you don't need them.
+    PrimaryComponentTick.bCanEverTick = true;
 
-	// ...
+    // ...
 }
 
 bool UDittoOutfits::CheckItemCompatible_Implementation(const TScriptInterface<ICombeeItemInterface>& Item,
                                                        const int32& Index) const
 {
-	return Super::CheckItemCompatible_Implementation(Item, Index);
+    const auto Fragment = Item->FindFragmentByClass<UDittoFragment_OutfitPart>();
+    return Fragment && UCombeeLibrary::IsNativeHost(Item, Fragment);
 }
 
 
 // Called when the game starts
 void UDittoOutfits::BeginPlay()
 {
-	if (GetOwner()->HasAuthority())
-	{
-		this->PostCellChange.AddDynamic(this, &ThisClass::HandleAuthorityCellMutation);
-	}
-	Super::Super::BeginPlay();
+    if (GetOwner()->HasAuthority())
+    {
+        this->PostCellChange.AddDynamic(this, &ThisClass::HandleAuthorityCellMutation);
+    }
+    Super::Super::BeginPlay();
 
-	// ...
+    // ...
 }
 
 void UDittoOutfits::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const
 {
-	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
-	FDoRepLifetimeParams Params;
-	Params.bIsPushBased = true;
-	Params.Condition = COND_OwnerOnly;
-	DOREPLIFETIME_WITH_PARAMS(ThisClass, Layout, Params);
-	DOREPLIFETIME_WITH_PARAMS(ThisClass, Avatar, Params);
+    Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+    FDoRepLifetimeParams Params;
+    Params.bIsPushBased = true;
+    Params.Condition = COND_OwnerOnly;
+    DOREPLIFETIME_WITH_PARAMS(ThisClass, Layout, Params);
+    DOREPLIFETIME_WITH_PARAMS(ThisClass, Avatar, Params);
 }
 
 void UDittoOutfits::SetAvatar(AActor* InAvatar)
 {
-	if (GetOwner()->HasAuthority() && InAvatar != Avatar)
-	{
-		ResetLayout();
+    if (GetOwner()->HasAuthority() && InAvatar != Avatar)
+    {
+        ResetLayout();
 
-		if (IsValid(InAvatar) && InAvatar->Implements<UDittoOutfitAvatar>())
-		{
-			COMPARE_ASSIGN_AND_MARK_PROPERTY_DIRTY(ThisClass, Avatar, InAvatar, this);
-			IDittoOutfitAvatar::Execute_SetupOutfitAvatar(Avatar, this);
-		}
-		else
-		{
-			COMPARE_ASSIGN_AND_MARK_PROPERTY_DIRTY(ThisClass, Avatar, nullptr, this);
-		}
-	}
+        if (IsValid(InAvatar) && InAvatar->Implements<UDittoOutfitAvatar>())
+        {
+            COMPARE_ASSIGN_AND_MARK_PROPERTY_DIRTY(ThisClass, Avatar, InAvatar, this);
+            IDittoOutfitAvatar::Execute_SetupOutfitAvatar(Avatar, this);
+        }
+        else
+        {
+            COMPARE_ASSIGN_AND_MARK_PROPERTY_DIRTY(ThisClass, Avatar, nullptr, this);
+        }
+    }
 }
 
 void UDittoOutfits::OnRep_Avatar_Implementation()
@@ -75,61 +79,61 @@ void UDittoOutfits::OnReq_Layout_Implementation()
 
 FDittoOutfitLayoutPart UDittoOutfits::RegisterPart(FGameplayTagContainer Part, const FInstancedStruct& PartData)
 {
-	const auto Info = this->Layout.Register(Part, PartData);
-	MARK_PROPERTY_DIRTY_FROM_NAME(ThisClass, Layout, this);
-	this->Hive.Cells.SetNum(Layout.PartsRegistry.Num());
-	this->Hive.Cells.Last().Init(this, Info.ContainerIndex);
-	return Info;
+    const auto Info = this->Layout.Register(Part, PartData);
+    MARK_PROPERTY_DIRTY_FROM_NAME(ThisClass, Layout, this);
+    this->Hive.Cells.SetNum(Layout.PartsRegistry.Num());
+    this->Hive.Cells.Last().Init(this, Info.ContainerIndex);
+    return Info;
 }
 
 void UDittoOutfits::ResetLayout()
 {
-	Layout.Reset();
-	this->Hive.Cells.SetNum(Layout.PartsRegistry.Num());
+    Layout.Reset();
+    this->Hive.Cells.SetNum(Layout.PartsRegistry.Num());
 }
 
 FDittoOutfitLayoutPart UDittoOutfits::RetrievePartInfo(const int32 Index) const
 {
-	if (Layout.PartsRegistry.IsValidIndex(Index))
-	{
-		return *Layout.PartsRegistry[Index].GetPtr<FDittoOutfitLayoutPart>();
-	}
-	return {};
+    if (Layout.PartsRegistry.IsValidIndex(Index))
+    {
+        return *Layout.PartsRegistry[Index].GetPtr<FDittoOutfitLayoutPart>();
+    }
+    return {};
 }
 
 void UDittoOutfits::FindIndexByPart(const FGameplayTagContainer& Part, TArray<int32>& OutIndices, int32& First,
                                     int32& Last, bool& Found) const
 {
-	First = INDEX_NONE;
-	Last = INDEX_NONE;
-	OutIndices.Empty();
+    First = INDEX_NONE;
+    Last = INDEX_NONE;
+    OutIndices.Empty();
 
-	for (int Index = 0; Index < Layout.PartsRegistry.Num(); ++Index)
-	{
-		const auto PartInfo = Layout.PartsRegistry[Index];
-		if (UBlueprintGameplayTagLibrary::HasAllTags(PartInfo.GetPtr()->Part, Part, true))
-		{
-			const int32 ContainerIndex = PartInfo.GetPtr()->ContainerIndex;
-			check(CheckIndexIsValid(ContainerIndex))
-			OutIndices.Add(ContainerIndex);
-		}
-	}
+    for (int Index = 0; Index < Layout.PartsRegistry.Num(); ++Index)
+    {
+        const auto PartInfo = Layout.PartsRegistry[Index];
+        if (UBlueprintGameplayTagLibrary::HasAllTags(PartInfo.GetPtr()->Part, Part, true))
+        {
+            const int32 ContainerIndex = PartInfo.GetPtr()->ContainerIndex;
+            check(CheckIndexIsValid(ContainerIndex))
+            OutIndices.Add(ContainerIndex);
+        }
+    }
 
-	Found = OutIndices.Num() > 0;
-	if (Found)
-	{
-		First = OutIndices[0];
-		Last = OutIndices.Last();
-	}
+    Found = OutIndices.Num() > 0;
+    if (Found)
+    {
+        First = OutIndices[0];
+        Last = OutIndices.Last();
+    }
 }
 
 void UDittoOutfits::HandleAuthorityCellMutation(const FCombeeCellMutationContext& Context)
 {
-	if (Context.State == ECombeeExecutionState::Success)
-	{
-		if (IsValid(Avatar) && Avatar->Implements<UDittoOutfitAvatar>())
-		{
-			IDittoOutfitAvatar::Execute_ReceiveOutfitUpdate(Avatar, this, Context);
-		}
-	}
+    if (Context.State == ECombeeExecutionState::Success)
+    {
+        if (IsValid(Avatar) && Avatar->Implements<UDittoOutfitAvatar>())
+        {
+            IDittoOutfitAvatar::Execute_ReceiveOutfitUpdate(Avatar, this, Context);
+        }
+    }
 }
--- Source/Ditto/Outfit/DittoOutfits.h
+++ Source/Ditto/Outfit/DittoOutfits.h
@@ -12,59 +12,59 @@
 UCLASS(ClassGroup=(Outfit), meta=(BlueprintSpawnableComponent))
 class DITTO_API UDittoOutfits : public UCombeeContainer
 {
-	GENERATED_BODY()
+    GENERATED_BODY()
 
 public:
-	// Sets default values for this component's properties
-	UDittoOutfits();
-	virtual bool CheckItemCompatible_Implementation(const TScriptInterface<ICombeeItemInterface>& Item,
-	                                                const int32& Index) const override;
+    // Sets default values for this component's properties
+    UDittoOutfits();
+    virtual bool CheckItemCompatible_Implementation(const TScriptInterface<ICombeeItemInterface>& Item,
+                                                    const int32& Index) const override;
 
 protected:
-	// Called when the game starts
-	virtual void BeginPlay() override;
-	virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
+    // Called when the game starts
+    virtual void BeginPlay() override;
+    virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
 
 public:
-	UPROPERTY(ReplicatedUsing=OnRep_Avatar, BlueprintReadWrite, Category="Outfit", Setter)
-	TObjectPtr<AActor> Avatar;
+    UPROPERTY(ReplicatedUsing=OnRep_Avatar, BlueprintReadWrite, Category="Outfit", Setter)
+    TObjectPtr<AActor> Avatar;
 
-	UPROPERTY(ReplicatedUsing=OnReq_Layout, BlueprintReadOnly, Category="Outfit")
-	FDittoOutfitLayout Layout = FDittoOutfitLayout(this);
+    UPROPERTY(ReplicatedUsing=OnReq_Layout, BlueprintReadOnly, Category="Outfit")
+    FDittoOutfitLayout Layout = FDittoOutfitLayout(this);
 
-	UFUNCTION(BlueprintAuthorityOnly)
-	void SetAvatar(AActor* InAvatar);
+    UFUNCTION(BlueprintAuthorityOnly)
+    void SetAvatar(AActor* InAvatar);
 
-	UFUNCTION(BlueprintNativeEvent)
-	void OnRep_Avatar();
+    UFUNCTION(BlueprintNativeEvent)
+    void OnRep_Avatar();
 
-	UFUNCTION(BlueprintNativeEvent)
-	void OnReq_Layout();
+    UFUNCTION(BlueprintNativeEvent)
+    void OnReq_Layout();
 
-	UFUNCTION(BlueprintCallable, Category="Outfit", meta=(AutoCreateRefTerm="PartData"), BlueprintAuthorityOnly)
-	FDittoOutfitLayoutPart RegisterPart(FGameplayTagContainer Part,
-	                                    const FInstancedStruct& PartData = FInstancedStruct());
+    UFUNCTION(BlueprintCallable, Category="Outfit", meta=(AutoCreateRefTerm="PartData"), BlueprintAuthorityOnly)
+    FDittoOutfitLayoutPart RegisterPart(FGameplayTagContainer Part,
+                                        const FInstancedStruct& PartData = FInstancedStruct());
 
-	UFUNCTION(BlueprintCallable, Category="Outfit", BlueprintAuthorityOnly)
-	void ResetLayout();
+    UFUNCTION(BlueprintCallable, Category="Outfit", BlueprintAuthorityOnly)
+    void ResetLayout();
 
-	UFUNCTION(BlueprintPure, Category="Outfit")
-	FDittoOutfitLayoutPart RetrievePartInfo(int32 Index) const;
+    UFUNCTION(BlueprintPure, Category="Outfit")
+    FDittoOutfitLayoutPart RetrievePartInfo(int32 Index) const;
 
-	UFUNCTION(BlueprintPure, Category="Outfit", meta=(AutoCreateRefTerm="Part"))
-	void FindIndexByPart(const FGameplayTagContainer& Part,
-	                     TArray<int32>& OutIndices,
-	                     int32& First,
-	                     int32& Last,
-	                     bool& Found) const;
+    UFUNCTION(BlueprintPure, Category="Outfit", meta=(AutoCreateRefTerm="Part"))
+    void FindIndexByPart(const FGameplayTagContainer& Part,
+                         TArray<int32>& OutIndices,
+                         int32& First,
+                         int32& Last,
+                         bool& Found) const;
 
-	UFUNCTION(BlueprintPure, Category="Outfit")
-	static bool CheckPartInfoValid(const FDittoOutfitLayoutPart& Info)
-	{
-		return Info.IsValid();
-	}
+    UFUNCTION(BlueprintPure, Category="Outfit")
+    static bool CheckPartInfoValid(const FDittoOutfitLayoutPart& Info)
+    {
+        return Info.IsValid();
+    }
 
 protected:
-	UFUNCTION()
-	void HandleAuthorityCellMutation(const FCombeeCellMutationContext& Context);
+    UFUNCTION()
+    void HandleAuthorityCellMutation(const FCombeeCellMutationContext& Context);
 };
